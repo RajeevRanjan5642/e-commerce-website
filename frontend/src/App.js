258a1506e30525ef81af8eb946784f3d5f8db007
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-10-10 08:34:18 +0530

    add hero component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,14 +12,14 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop/>}/>
-          <Route path="/men" element={<ShopCategory category='men'/>}/>
-          <Route path="/women" element={<ShopCategory category='women'/>}/>
-          <Route path="/kids" element={<ShopCategory category='kids'/>}/>
-          <Route path="/product" element={<Product/>}/>
-          <Route path="/product/:id" element={<Product/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/login" element={<LoginSignup/>}/>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="men" />} />
+          <Route path="/women" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -26,5 +26,5 @@ const Navbar = () => {
         </div>
      );
 }
- 
+
 export default Navbar;
