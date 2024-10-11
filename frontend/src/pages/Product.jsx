5a9c4e1e2c87480f7acebc1ced893d63bc9345c4
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-10-11 20:01:43 +0530

    create structure of productdisplay

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import {ShopContext} from './../context/ShopContext'
 import {useParams} from 'react-router-dom';
 import Breadcrumb from "./../components/Breadcrumbs/Breadcrumbs"
+import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
     const {all_product} = useContext(ShopContext)
@@ -10,6 +11,7 @@ const Product = () => {
     return ( 
         <div>
             <Breadcrumb product={product}/>
+            <ProductDisplay product={product}/>
         </div> 
     );
 }
