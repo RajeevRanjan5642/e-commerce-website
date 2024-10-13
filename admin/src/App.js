2402e2d32c0bc1c002f1c15204163f29e202da08
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-10-13 23:25:12 +0530

    create sidebar

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Admin from './pages/Admin/Admin'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Admin/>
     </div>
   );
 }
