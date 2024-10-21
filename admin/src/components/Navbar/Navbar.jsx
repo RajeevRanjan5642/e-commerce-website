8b6d8df309817df71c95d744faba45fc8de9d5cd
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-10-21 20:25:40 +0530

    fix issue

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './Navbar.css'
 import navlogo from './../../assets/nav-logo.svg'
-import navProfile from './../../assets/nav-profile.svg'
+import profile_icon from './../../assets/profile_icon.png'
 
 const Navbar = () => {
     return ( 
         <div className="navbar">
             <img src={navlogo} alt="" className="nav-logo" />
-            <img src={navProfile} alt="" className="nav-profile"/>
+            <img src={profile_icon} alt="" className="nav-profile"/>
         </div> 
     );
 }
--- frontend/src/components/Offers/Offers.jsx
+++ frontend/src/components/Offers/Offers.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Offers.css'
 import exclusive_image from '../assets/exclusive_image.png'
+import { useNavigate } from 'react-router-dom';
 
 const Offers = () => {
+    const navigate = useNavigate();
     return ( 
         <div className="offers">
             <div className="offers-left">
                 <h1>Exclusive</h1>
                 <h1>Offers for you</h1>
                 <p>ONLY ON BEST SELLERS PRODUCTS</p>
-                <button>Check Now</button>
+                <button onClick={()=>navigate('/men')}>Check Now</button>
             </div>
             <div className="offers-right">
                 <img src={exclusive_image} alt="" />
